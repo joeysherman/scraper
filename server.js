d1c6e6065c8137784a184cb96a98394ad91c58bd
Joey Sherman <joey.a.sherman@gmail.com>
2017-03-13 21:27:58 -0700

    - add chalk - start on simple scraper

--- server.js
+++ server.js
@@ -1,3 +1,40 @@
 /**
  * Created by Joey on 3/10/2017.
  */
+
+
+const cheerio = require('cheerio'),
+      request = require('request-promise'),
+      logger  = console.Console;
+
+logger.newline = function() {
+  console.log('---------------------------------');
+};
+
+logger.log = function(data) {
+  console.log(data);
+}
+
+logger.newline();
+
+/**
+ *
+ * @param url
+ */
+function scrape(url){
+  request(url)
+    .then(function(body) {
+      return cheerio.load(body);
+    })
+    .then(function($) {
+      $('ul > li > a').each(function(i, elem) {
+
+        console.log('#' + i + ' - ' + $(this).text());
+      });
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+};
+
+scrape('https://en.wikipedia.org/wiki/Lists_of_American_institutions_of_higher_education');
