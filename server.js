602aa2d0f6a0cb65c68759dcf5ef083da52674a7
Joey Sherman <joey.a.sherman@gmail.com>
2017-03-16 02:01:03 -0700

    - Add moment - Begin adding support for streams

--- server.js
+++ server.js
@@ -5,22 +5,62 @@
 
 const cheerio = require('cheerio'),
       request = require('request-promise'),
+      fs      = require('fs'),
+      chalk   = require('chalk'),
+      moment  = require('moment'),
       logger  = console.Console;
 
+
+var   date = moment().format('MMMDoYYYY'),
+      time = moment().format('LT'),
+      filename = date + '.csv',
+      stream = {};
+
+logger.init = function() {
+  logger.newline();
+  console.log('Scraper ' + chalk.green('started') + ' - ' + chalk.blue(time));
+  logger.newline();
+};
+
 logger.newline = function() {
-  console.log('---------------------------------');
+  console.log('---------------------------------\n');
 };
 
 logger.log = function(data) {
-  console.log(data);
-}
+  console.log(data + '/n');
+};
 
-logger.newline();
+logger.init();
 
 /**
  *
  * @param url
  */
+
+if (fs.existsSync(filename)) {
+  filename = date + '-' + Date.now() + '.csv';
+}
+
+stream = fs.createWriteStream(filename);
+
+
+stream.on('flush', function() {
+  logger.log('flushing');
+});
+
+stream.on('flush', function() {
+  logger.log('flushing');
+});
+
+stream.on('flush', function() {
+  logger.log('flushing');
+});
+
+stream.on('flush', function() {
+  logger.log('flushing');
+});
+
+
 function scrape(url){
   request(url)
     .then(function(body) {
